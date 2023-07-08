9151ce68798b5f3bd6a1c93ab0cc5d0580eff01c
TanTan1941 <105098069+TanTan1941@users.noreply.github.com>
2023-07-08 09:11:37 -0700

    Delete csv_user_add.py

--- csv_user_add.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-user = input('First name:')
-passwd = input('Last name: ')
-
-csvfile = open('names.csv', mode='w')
-fieldnames = ['first_name', 'last_name']
-writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-writer.writerow({'first_name': user, 'last_name': passwd})
