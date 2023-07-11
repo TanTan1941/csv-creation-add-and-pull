ed721724b62de43aedca1bf0431762d848e7ed1b
TanTan1941 <105098069+TanTan1941@users.noreply.github.com>
2023-07-11 16:39:38 -0700

    Delete csv_create.py

--- csv_create.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('names.csv' , mode="w") as csvfile:
-    fieldnames = ['first_name', 'last_name']
-    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-    writer.writeheader()
-    writer.writerow({"first_name": "john", "last_name": "doe"})
-    writer.writerow({"first_name": 'jane', "last_name": "doe"})
